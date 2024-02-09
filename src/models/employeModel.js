efa52ea04baa00432c11cc0d2f8b34f80b28a193
Mendrik-1405 <herilanto5.0k@gmail.com>
2024-02-09 15:39:15 +0300

    creation model

--- src/models/employeModel.js
+++ src/models/employeModel.js
@@ -1,10 +1,20 @@
 const mongoose = require('../utils/db');
+const service = require("./serviceModel");
+const tache = require("./tacheModel");
 
     employeSchema = new mongoose.Schema({
-        name: String,
+        nom: String,
+        email: String,
         password: String,
-        role: String,
-        phone: String
+        phone: String,
+        sexe: String,
+        photo: String,
+        services:[service.schema],
+        taches:[tache.schema],
+        horaire:{
+            debut:Date,
+            fin:Date
+        }
     })
 
 
