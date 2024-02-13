fc7f26e2ce908e4cf44ca9821a578e130019d8f7
MandresyRandriamarosoa <151185727+MandresyRandriamarosoa@users.noreply.github.com>
2024-02-13 12:40:48 +0300

    clientRoute

---
+++ src/routes/clientRoutes.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const clientController = require('../controllers/clientController');
+const {verifyToken} =require('../middlewares/authentification')
+
+const router = express.Router();
+
+// Définir les routes pour gérer les services
+router.get('/:id', clientController.findById);
+router.get('/',verifyToken, clientController.findAll);
+router.post('/', clientController.create);
+router.put('/:id', clientController.update);
+router.post('/login', clientController.login);
+router.delete('/delete/:id', clientController.delete);
+
+module.exports = router;
