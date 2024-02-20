a57633e1f5f02bc6b2d0e9abf44e99a809717df9
Mendrik-1405 <herilanto5.0k@gmail.com>
2024-02-20 18:21:13 +0300

    [fix] cors origin

--- app.js
+++ app.js
@@ -2,10 +2,19 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cookieParser=require('cookie-parser');
+const cors = require('cors');
 require('dotenv').config();
 
 const app = express();
 const port = process.env.PORT || 5000;
+
+const corsOptions = {
+  origin: '*',
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  optionsSuccessStatus: 200
+};
+
+app.use(cors(corsOptions));
 
 app.use(cookieParser())
 app.use(bodyParser.json());
@@ -16,7 +25,7 @@ const allRoutes = require('./src/routes/index');
 async function use_routes(routes){
   Object.keys(routes)
   .forEach((key) => {
-      app.use(`/${key}`, routes[key]);
+      app.use(`/api/${key}`, routes[key]);
   });
 }
 
